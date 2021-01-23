fa16c504863832b80e45add7e32ec4592f710507
PaltusovaEvgeniya <Ev.Paltusova@gmail.com>
2021-01-23 21:41:52 +0300

    слайдеры начало

--- js/script.js
+++ js/script.js
@@ -1,8 +1,45 @@
-var mySwiper = new Swiper('.swiper-container', {
-	// Optional parameters
+var swiper = new Swiper('.swiper-container', {
 	direction: 'horizontal',
-	loop: true,
-	slidesPerView: 4,
-	spaceBetween: 30,
+	// loop: true,
+	slidesPerView: 2,
+	spaceBetween: 10,
+	// slidesPerGroup: 2,
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+		clickable: true,
+	},
+	breakpoints: {
 
-})
+		320: {
+			slidesPerView: 2,
+			spaceBetween: 10,
+			slidesPerGroup: 2,
+
+		},
+		480: {
+			slidesPerView: 3,
+			spaceBetween: 20,
+			slidesPerGroup: 2,
+
+		},
+		640: {
+			slidesPerView: 4,
+			spaceBetween: 30,
+			slidesPerGroup: 2,
+
+		}
+	}
+});
+
+var swiper2 = new Swiper('.swiper-container_2', {
+	direction: 'horizontal',
+	// loop: true,
+	slidesPerView: 2,
+	spaceBetween: 50,
+	pagination: {
+		el: '.swiper-pagination',
+		type: 'bullets',
+		clickable: true,
+	},
+});

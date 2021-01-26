8d3c64ce63b2e445d22658b58d9846806fcfad10
PaltusovaEvgeniya <Ev.Paltusova@gmail.com>
2021-01-26 22:25:11 +0300

    слайдеры для проверки на ошибки Настей

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 var swiper = new Swiper('.swiper-container', {
 	direction: 'horizontal',
 	loop: true,
-	slidesPerView: 2,
+	slidesPerView: 4,
 	spaceBetween: 10,
 	slidesPerGroup: 2,
 	pagination: {
@@ -14,54 +14,49 @@ var swiper = new Swiper('.swiper-container', {
 		320: {
 			slidesPerView: 2,
 			spaceBetween: 15,
-			slidesPerGroup: 2,
+			slidesPerGroup: 2
 
 		},
 		480: {
 			slidesPerView: 3,
 			spaceBetween: 20,
-			slidesPerGroup: 2,
+			slidesPerGroup: 2
 
 		},
 		640: {
+			slidesPerView: 3,
+			spaceBetween: 30,
+			slidesPerGroup: 2
+
+		},
+		780: {
 			slidesPerView: 4,
 			spaceBetween: 30,
-			slidesPerGroup: 2,
+			slidesPerGroup: 2
 
-		}
+		},
 	}
 });
 
 var swiper2 = new Swiper('.swiper-container_2', {
 	direction: 'horizontal',
 	slidesPerView: 2,
-	spaceBetween: 10,
+	spaceBetween: 70,
 	pagination: {
 		el: '.pag__2',
 		type: 'bullets',
 		clickable: true,
 	},
+	// если разкомментировать брейкпоинты и поставить slidesPerView: 1, то почему-то пропадают отступы в 70, хотя без брейкпоинтов все работает
 	// breakpoints: {
 
-	// 	320: {
-	// 		slidesPerView: 1,
-	// 		spaceBetween: 10,
-	// 	},
-	// 	480: {
-	// 		slidesPerView: 1,
-	// 		spaceBetween: 30,
-	// 	},
-	// 	650: {
-	// 		slidesPerView: 2,
-	// 		spaceBetween: 30,
-	// 	}
 	// 	768: {
 	// 		slidesPerView: 2,
 	// 		spaceBetween: 30,
 	// 	}
 	// 	992: {
 	// 		slidesPerView: 2,
-	// 		spaceBetween: 60,
+	// 		spaceBetween: 70,
 	// 	}
 	// }
 });
@@ -75,12 +70,24 @@ var swiper3 = new Swiper('.swiper-container_3', {
 		type: 'bullets',
 		clickable: true,
 	},
+	// если раскомментировать брейкпоинты то пропадают и буллеты и ломаются все отступы
+	// breakpoints: {
+
+	// 	468: {
+	// 		slidesPerView: 2,
+	// 		spaceBetween: 30,
+	// 	}
+	// 	992: {
+	// 		slidesPerView: 3,
+	// 		spaceBetween: 30,
+	// 	}
+	// }
 });
 
-
+// а с эти вообще непонятки, почему-то все слайды в одну строчку встают сразу, хотя им задано что три в превью и flex-basis в 33%
 var swiper4 = new Swiper('.swiper-container_4', {
 	direction: 'horizontal',
-	slidesPerView: 2,
+	slidesPerView: 3,
 	spaceBetween: 50,
 	pagination: {
 		el: '.pag__4',
